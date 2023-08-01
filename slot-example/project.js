620fa5bc25531530d878e20045020fb506030b89
Aleksandar Ristic <storiaca@gmail.com>
2023-08-01 08:13:22 +0200

    Create deposit function

--- slot-example/project.js
+++ slot-example/project.js
@@ -1,1 +1,24 @@
 // 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+const promt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = promt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+deposit();
