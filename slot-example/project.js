f65d4f69a0ed1c85de28c08bb6808799ce1d8aae
Aleksandar Ristic <storiaca@gmail.com>
2023-10-21 12:20:11 +0200

    Get winnings

--- slot-example/project.js
+++ slot-example/project.js
@@ -97,11 +97,44 @@ const transpose = (reels) => {
   return rows;
 };
 
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "";
+    for (const [i, symbol] of row.entries()) {
+      rowString += symbol;
+      if (i != row.length - 1) {
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+}
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row]; 
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+  return winnings;
+}
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
-
+printRows(rows)
 console.log(reels);
 console.log(rows);
+// 51:13
